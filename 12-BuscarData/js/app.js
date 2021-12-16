8c3fc2c2d168c48b5e71985ba306bd4f9f958607
diegotpereira <diegoteixeirapereira@hotmail.com>
2021-12-16 16:53:23 -0300

    criação app javascript com exemplo de exibir data atual.

---
+++ 12-BuscarData/js/app.js
@@ -0,0 +1,6 @@
+var dias = new Array('Domingo', 'Segunda', 'terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado')
+
+var hoje = new Date()
+var diaSemana = hoje.getDay()
+
+alert('Bem vindo, hoje é ' + dias[4])
