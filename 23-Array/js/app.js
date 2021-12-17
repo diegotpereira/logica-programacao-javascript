7a2c3371af49900bd7077122310bb1907b9f6f8b
diegotpereira <diegoteixeirapereira@hotmail.com>
2021-12-17 08:59:34 -0300

    criação do app javascript com exemplo de dobra de Array.

---
+++ 23-Array/js/app.js
@@ -0,0 +1,18 @@
+var arr = [1, 3, 6, 9]
+var arrDobrado = dobrar(arr)
+
+function dobrar(arr) {
+
+    var arrTemp = []
+
+    for (var i = 0; i < arr.length; i++) {
+
+        arrTemp[i] = arr[i] * 2
+    }
+
+
+    return arrTemp
+}
+
+console.log(arr)
+console.log(arrDobrado)
